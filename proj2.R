1faffcdc9f67055b98cd6a9f933ea98c2d718d76
rauldoe <khoado@csu.fullerton.edu>
2019-12-19 20:05:30 -0800

    fixed SPARK code

--- proj2.R
+++ proj2.R
@@ -3,10 +3,12 @@
 # library(sparklyr)
 # spark_install()
 
+library(tidyverse)
 library(sparklyr)
 
-mylocaldata <- read_csv ("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
-library(sparklyr)
+# mylocaldata <- read_csv ("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
+mylocaldata <- read_csv("Bodyfat.csv")
+
 sc <- spark_connect(master = "local")
 myremotedata <- copy_to(sc, mylocaldata)
 mymodel <- ml_linear_regression(x=myremotedata , formula = bodyfat ~ Weight + Height)
