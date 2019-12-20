baf897d0c9c44f606cbc6a93ec555be6173703fb
Khoa Do <kdo1@costar.com>
2019-12-20 11:32:04 -0800

    fixed spark code and final submission

--- proj2.R
+++ proj2.R
@@ -12,8 +12,13 @@ setwd("c:/temp/cpsc375proj2/")
 mylocaldata <- read_csv("Bodyfat.csv")
 
 sc <- spark_connect(master = "local")
-myremotedata <- copy_to(sc, mylocaldata)
-mymodel <- ml_linear_regression(x=myremotedata , formula = bodyfat ~ Weight + Height)
+myremotedata <- copy_to(sc, mylocaldata, overwrite = TRUE)
+# our group's formula for the best model that describes bodyfat
+# bodyfat ~ Wrist + log(Abdomen) + Weight^2
+# unfortunately, ml_linear_regression has a problem with the log and exponent functions \
+# so we have to simplify the formula
+mymodel <- ml_linear_regression(x=myremotedata , formula = bodyfat~Wrist+Abdomen+Weight)
+
 summary(mymodel)
 
 spark_web(sc)
